edb5cb312139d942fc1af16783910d3217f5b09e
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-18 14:50:24 -0800

    Baseline requirements complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+$(document).ready(function() {
+
+  var clock = setInterval(function() {
+    var currentTime = new Date();
+    var time = currentTime.toLocaleTimeString('en-US');
+    // var minutes = currentTime.getMinutes().toLocaleTimeString( "America/Los_Angeles");
+    // var seconds = currentTime.getSeconds().toLocaleTimeString( "America/Los_Angeles");
+
+    var clockFace = $(
+      '<p>' + time + '</p>'
+    );
+    $('#clock').html(clockFace);
+  }, 1000);
+});
